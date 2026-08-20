dba681493c6abd8d4a24ca8dc0b9398aff012535
Gaur-Divyansh <179626748+Gaur-Divyansh@users.noreply.github.com>
2026-08-20 20:30:30 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.cpp
@@ -0,0 +1,61 @@
+class Node{
+    public: 
+    int val;
+    int key;
+    Node* next;
+    Node* prev;
+    Node(int k, int v) : key(k),val(v),next(nullptr),prev(nullptr) {}
+};
+
+class LRUCache {
+private:
+    int cap;
+    unordered_map<int, Node*> cache;
+    Node* left;
+    Node* right;
+    void remove(Node* node){
+        Node* prev = node->prev;
+        Node* next = node->next;
+        prev->next = next;
+        next->prev = prev;
+    }
+    void insert(Node* node){
+        Node* prev = right->prev;
+        prev->next = node;
+        node->prev = prev;
+        node->next = right;
+        right->prev = node;
+    }
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+        cache.clear();
+        left = new Node(0,0);
+        right= new Node(0,0);
+        left->next = right;
+        right->prev = left;
+    }
+    
+    int get(int key) {
+        if(cache.count(key)){
+            Node* node = cache[key];
+            remove(node);
+            insert(node);
+            return node->val;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if(cache.count(key)) remove(cache[key]);
+        Node* newNode = new Node(key,value);
+        cache[key] = newNode;
+        insert(newNode);
+        if(cache.size() > cap){
+            Node* lru = left->next;
+            remove(lru);
+            cache.erase(lru->key);
+            delete lru;
+        }
+    }
+};
