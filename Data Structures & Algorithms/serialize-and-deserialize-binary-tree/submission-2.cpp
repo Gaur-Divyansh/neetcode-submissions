e676a5f08e7ba612a15373db1cc084208adb7712
Gaur-Divyansh <179626748+Gaur-Divyansh@users.noreply.github.com>
2026-09-15 13:14:41 +0530

    Add: serialize-and-deserialize-binary-tree - submission-2

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-2.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Codec {
+    TreeNode* deserialize_helper(queue<string>& q){
+        string s = q.front();
+        q.pop();
+        if(s == "NULL") return NULL;
+        TreeNode* node = new TreeNode(stoi(s));
+        node->left = deserialize_helper(q);
+        node->right = deserialize_helper(q);
+        return node;
+    }
+public:
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if(!root) return "NULL,";
+        return to_string(root->val) + "," + serialize(root->left) + serialize(root->right);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        queue<string> q;
+        string s;
+        for(int i = 0; i < data.size();i++){
+            if(data[i] == ','){
+                q.push(s);
+                s = "";
+                continue;
+            }
+            s += data[i];
+        }
+        if(s.size() != 0) q.push(s);
+        return deserialize_helper(q);
+    }
+};
